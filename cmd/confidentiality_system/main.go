0a6ef2d9d7023f6b358b29d85c3176ef12650108
Daniel Bahrami <daniel@bahrami.dk>
2025-03-29 16:42:05 +0100

    Connect to postgres

--- cmd/confidentiality_system/main.go
+++ cmd/confidentiality_system/main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/danielbahrami/se10-mt/internal/postgres"
+)
+
+func main() {
+	postgres.ConnectPostgres()
+
+	i := 1
+	for {
+		fmt.Println(i)
+		i++
+		time.Sleep(10 * time.Second)
+	}
+}
--- internal/postgres/postgres.go
+++ internal/postgres/postgres.go
@@ -1,1 +1,23 @@
 package postgres
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v5"
+)
+
+func ConnectPostgres() {
+	DATABASE_URL := "postgres://" + os.Getenv("POSTGRES_USER") + ":" + os.Getenv("POSTGRES_PASSWORD") + "@" +
+		os.Getenv("POSTGRES_HOST") + ":" + os.Getenv("POSTGRES_PORT") + "/" + os.Getenv("POSTGRES_DB")
+	fmt.Println("DATABASE_URL: " + DATABASE_URL)
+
+	// urlExample := "postgres://username:password@localhost:5432/database_name"
+	conn, err := pgx.Connect(context.Background(), DATABASE_URL)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+}
